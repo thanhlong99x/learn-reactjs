a07f576b564183a755e7b2f27f4d4775491cc801
Nguyễn Thành Long <toilalong.99@gmail.com>
2021-07-22 17:50:31 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  
+  <h1>hello reactjs</h1>,
+  document.getElementById('root')
+);
+// tất cả các phần tử render ra DOM đều là đối tượng react tạo ra
